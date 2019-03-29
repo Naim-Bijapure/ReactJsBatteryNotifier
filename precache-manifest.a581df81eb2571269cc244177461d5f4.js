542ac4cacbb2813d647d717bf63fcc1fa1a529fb
Naim-Bijapure <Naimbijapure7407@gmail.com>
2019-03-29 14:26:26 +0530

    Updates

---
+++ precache-manifest.a581df81eb2571269cc244177461d5f4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "98db444ea25b77593bc2",
+    "url": "/ReactJsBatteryNotifier/static/js/main.b68aa509.chunk.js"
+  },
+  {
+    "revision": "2b3a108f6dcb3db11387",
+    "url": "/ReactJsBatteryNotifier/static/js/runtime~main.6c84cb85.js"
+  },
+  {
+    "revision": "4ca4a48c119429b9d516",
+    "url": "/ReactJsBatteryNotifier/static/js/2.f5b8218b.chunk.js"
+  },
+  {
+    "revision": "2c8d375483de15bd923f",
+    "url": "/ReactJsBatteryNotifier/static/js/3.48e1430c.chunk.js"
+  },
+  {
+    "revision": "80adbe0a70f9a4a7ea9f",
+    "url": "/ReactJsBatteryNotifier/static/js/4.4e30a200.chunk.js"
+  },
+  {
+    "revision": "b512d3b932f0d4a8d259",
+    "url": "/ReactJsBatteryNotifier/static/js/5.2b551b00.chunk.js"
+  },
+  {
+    "revision": "4ceba4d5e391321aa2b8",
+    "url": "/ReactJsBatteryNotifier/static/js/6.b404d5ac.chunk.js"
+  },
+  {
+    "revision": "fcc5a64262c72b8fa27e",
+    "url": "/ReactJsBatteryNotifier/static/js/7.3ad92bab.chunk.js"
+  },
+  {
+    "revision": "c27cbeed0952963fa313c76990812e75",
+    "url": "/ReactJsBatteryNotifier/static/media/ic_launcher.c27cbeed.png"
+  },
+  {
+    "revision": "bed4dac14faaccb3e37d652041a6cefd",
+    "url": "/ReactJsBatteryNotifier/index.html"
+  }
+];
